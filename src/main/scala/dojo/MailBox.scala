b4d0975c98f42347a23eab1b74f679914a5fc801
Helen Williamson <helen.williamson@playfish.com>
2012-03-06 09:43:23 +0000

    Fixed tests

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,9 +3,19 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
-    case _ => None
+    case Ping => Pong
+    case Pong => Ping
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) =
+  {
+    val something = "echo"
+    msg match {
+    case pingPong:PingPongMessage => handleMsg(pingPong)
+    case "stop" => "stopped"
+    case (something, echoMessage) => echoMessage
+  }
+  }
+
 
 }
